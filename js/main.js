15b064248bb5a2fcea261f6e69e5bf38b7cbf5db
styura3000 <styura3000@gmail.com>
2021-10-22 15:31:54 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function(){
+   $(':submit').click(function(e) {
+      $('.text2').attr('disabled','disabled');
+      e.preventDefault();
+   });
+   $(':reset').click(function(e) {
+      $('.text2').removeAttr('disabled');
+      e.preventDefault();
+   });
+});
+
+
+
+$(document).ready(function () {
+
+});
+
+$(function () {
+   $('.icons > div').hide(2000);
+});
+
+
